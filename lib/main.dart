a162ab3754c9d81c79d0a1482ed97c451ac5350c
Noel John Robert <noel060202@gmail.com>
2023-06-08 08:03:51 +0530

    added basic tiles

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,22 @@
 
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}): super(key: key);
 
@@ -72,15 +88,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> image = [
+    'https://cdn.pixabay.com/photo/2021/06/01/07/03/sparrow-6300790_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/10/20/10/58/elephant-2870777_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2014/09/08/17/32/humming-bird-439364_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2018/05/03/22/34/lion-3372720_960_720.jpg'];
+  List<String> title = ['Sparrow', 'Elephant', 'Humming Bird', 'Lion'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dhwani HomePage"),
       ),
-      body: ListView(
-        children: [],
+      // body: ListView.builder(
+      //   itemCount: image.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return card(image[index], title[index], context);
+      //   },
+      //   // children: [
+      //   //   Card(
+      //   //     child: Column(
+      //   //       children: [
+      //   //         Image.network(src),
+      //   //         Text(title)
+      //   //       ],
+      //   //     )
+      //   //   )
+      //   // ],
+      // )
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        itemCount: image.length,
+        itemBuilder: (BuildContext context, int index) {
+          return card(image[index], title[index], context);
+        },
       )
     );
   }
+}
+
+Widget card(String image, String title, BuildContext context) {
+  return Card(
+    color: Colors.yellow[50],
+    elevation: 8.0,
+    margin: EdgeInsets.all(4.0),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+    child: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Image.network(
+            image,
+            height: MediaQuery.of(context).size.width * (1/4),
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 12.0,
+            // fontWeight: FontWeight.w700
+          )
+        )
+      ],
+    ),
+  );
 }
